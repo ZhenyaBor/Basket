e67021b54337713028384a4aea6f99755e10c00b
Сергей Мельничук <Melnichuk@eds.local>
2022-07-01 14:06:46 +0300

    ReactTs

--- src/redux/slices/cardSlice.ts
+++ src/redux/slices/cardSlice.ts
@@ -11,30 +11,29 @@ const initialState: CardStateInterface = {
   products: [],
 };
 
-export const cardSlice = createSlice({
-  name: "card",
-  initialState,
-  reducers: {
-    setProductItem: (state, action: PayloadAction<ProductInterface>) => {
-      const productIndex = state.products.findIndex(
-        ({ id }) => id === action.payload.id
-      );
-      const prod = state.products[productIndex];
-      if (productIndex > -1) {
-        state.products[productIndex].count = prod.count ? prod.count + 1 : 0;
-
-        return state;
-      }
-      action.payload.count = 1;
-      state.products.push(action.payload);
-    },
-    setRemoveProductItem(state, action) {
-      state.products = state.products.filter(
-        (productItem) => productItem.id !== action.payload
-      );
-    },
-  },
-});
+    export const cardSlice = createSlice({
+      name: "card",
+      initialState,
+      reducers: {
+        setProductItem: (state, action: PayloadAction<ProductInterface>) => {
+          const productIndex = state.products.find(
+            ({ id }) => id === action.payload.id
+          );
+         
+          if (productIndex ) {
+            console.log(productIndex.price)
+          }else{
+            state.products.push(action.payload);
+          }
+      
+        },
+        setRemoveProductItem(state, action) {
+          state.products = state.products.filter(
+            (productItem) => productItem.id !== action.payload
+          );
+        },
+      },
+    });
 
 export const { setProductItem, setRemoveProductItem } = cardSlice.actions;
 
